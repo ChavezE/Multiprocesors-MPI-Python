5a40b21bbb975148a043fcf3c303393ac288e390
Sebastian Rivera <sebas.rivera96@gmail.com>
2019-03-26 14:58:32 -0600

    Create parallel implementation of Floyd Shortest Path algorithm
    The algorithm is in process to be finished. Some comments give insight
    of the future steps.

--- parallel/floydShortesPath.py
+++ parallel/floydShortesPath.py
@@ -3,6 +3,7 @@
 
 from mpi4py import MPI
 import numpy as np
+from math import sqrt
 import sys
 
 # ===== GLOBAL VARIABLES =====
@@ -13,88 +14,135 @@ TASK_MASTER = 0
 # ============================
 
 
-def floydWarshallOptimization(M):
+def floydWarshallOptimization(M, kRow, kCol):
     # M is a matrix with the costs of going through each road
     N = M.shape[0]
-    for k in range(N):
-            for i in range(N):
-                    for j in range(N):
-                            M[i][j] = min(M[i][j], M[i][k] + M[k][j])
-    print(M)
-    return M
+    for i in range(N):
+        for j in range(N):
+            M[i][j] = min(M[i][j], kCol[i] + kRow[j])
+    
+    # print(M) 
+
+def createMatrix(N):
+    rawMat = np.random.uniform(0, 100, (N,N))
+    roundedMat = np.round(rawMat, decimals=2)
+    return roundedMat
+
+def distributeMat(Mat):
+    sizeOfMat = Mat.shape
+    sizeOfSubMatrix = int(sizeOfMat[0] / sqrt(worldSize))
+    rankOfProcessor = 0
+    TMData = {}
+
+    for xOffset in range(0, sizeOfMat[0], sizeOfSubMatrix):
+        for yOffset in range(0, sizeOfMat[1], sizeOfSubMatrix):
+            # Compute the submatrix using offsets
+            thisMat = Mat[xOffset:xOffset+sizeOfSubMatrix, yOffset:yOffset+sizeOfSubMatrix]
+            initX = xOffset
+            initY = yOffset
+            data = {
+                'mat': thisMat,
+                'x', initX,
+                'y', initY
+            }
+            if rankOfProcessor == TASK_MASTER:
+                TMData = data
+            else:
+                comm.send(data, dest=rankOfProcessor, tag=rankOfProcessor)
+            rankOfProcessor += 1
+    
+    # Return the submatrix to Task Master
+    return TMData
+
+def computeSlicesOfK(M, initX, initY, k):
+    lengthOfM = M.shape[0]
+    kthRowSlice = None
+    kthColSlice = None
+
+    if k >= initX and k < initX + lengthOfM:
+        kthRowSlice = M[k,:]
+    if k >= initY and k < initY + lengthOfM:
+        kthColSlice = M[:,k]
+    
+    return kthRowSlice, kthColSlice
 
 
-def floydAlgorithm():
-    global comm
-    global rank
-    global worldSize
-    global TASK_MASTER
+def floydAlgorithm(sizeOfMat):
+    # The matrix used on the Floyd Washall algorithm is always square
 
-    totalRows = int(sys.argv[-2])
-    totalCols = int(sys.argv[-1])
-
-
+    # (I) == Distribute the Matrix from the Task Master to the remaining processors ==
     if rank == TASK_MASTER:
-        costMatrix = np.random.rand(totalRows, totalCols)
-        costMatrix[1] *= 100
-        costMatrix[3] *= 30
-
-        totalRows = costMatrix.shape[0]
-        # totalCols = costMatrix.shape[1]
+        weightsMatrix = createMatrix(sizeOfMat)
+        # print("== Original Weights Matrix ==")
+        # print(weightsMatrix)
         
-        rowsPerProcessor = int(totalRows / worldSize)
-        remainingRows = int(totalRows % worldSize)
-        rowsSent = []
-        offset = 0
+        # Distribute the Matrix in N/sqrt(worldSize) x N/sqrt(worldSize) matrices
+        data = distributeMat(weightsMatrix)
+        initX = data['x']
+        initY = data['y']
+        thisMat = data['mat']
+        # print("From rank {} ==> {}".format(rank, thisMat))
+    
+    # (II) == Each Processor that isn't the task master receives its own subMatrix ==
+    if rank != TASK_MASTER:
+        # Receive its corresponding submatrix, e.g. if sizeOfMat = 4 ==> submatrix is of 2x2
+        data = comm.recv(source=TASK_MASTER, tag=rank)
+        initX = data['x']
+        initY = data['y']
+        thisMat = data['mat']
 
-        print("\n== ORIGINAL MATRIX ==\n")
-        print(costMatrix)
+        # print("From rank {} ==> {}".format(rank, thisMat))
+
+    comm.Barrier()
+    # (III) == Loop for each value of k to compute the shortest paths ==
+
+    # ==> Iterate range(0 to k); bcast kRow and/or kCol slices
+    # TODO create a function, bcause this will be called from either ...
+    # ... TASK MASTER and the other processors
+    for k in range(0, sizeOfMat):
+        # These two arrays are placeholders to be used in future steps
+        kRow = None
+        kColumn = None
+        # a. If I have some slice of kthRow or kthColumn, broadcast it to all other processors
+        kRowSlice, kColSlice = computeSlicesOfK(thisMat, initX, initY, k)
+
+        # a.1 Arrange the data to be sent
+        # TODO move this section into a function
+        rowData2 = None
+        colData2 = None
+
+        if kRowSlice != None:
+            rowData1 = {
+                "row_slice": kRowSlice,
+                "x": initX
+            }
+        else:
+            rowData1 = None
+
+        if kColSlice != None:
+            colData1 = {
+                "col_slice": kColSlice,
+                "x": initY
+            }
+        else:
+            colData1 = None
+
+        rowData1 = comm.bcast(rowData1, )
+
+        # b. Recevie the slices of kthRow and kthColumn that I don't have
         
-        # Distribute the rows evenly within the processors
-        for rankOfProcessor in range(worldSize):
 
-            if rankOfProcessor == TASK_MASTER:
-                taskMasterBatch = costMatrix[offset:offset+rowsPerProcessor]
-                offset += rowsPerProcessor
-                rowsSent.append(rowsPerProcessor)
+        # c. Arrange the slices to build up the final kthRow and kthColumn
 
-            elif rankOfProcessor != TASK_MASTER:
-                thisRows = rowsPerProcessor
-                if remainingRows != 0:
-                    thisRows += 1
-                    remainingRows -= 1
+        # d. Call the function floydWarshallOptimization() for the kth time
+        # TODO verify that Python handles the Matrices as reference parameters
+        floydWarshallOptimization(thisMat, kRow, kCol)
 
-                batchRows = costMatrix[offset:offset+thisRows]
-                offset += thisRows
-                rowsSent.append(thisRows)
+    
 
-                comm.send(batchRows, dest=rankOfProcessor, tag=rankOfProcessor)
-
-        taskMasterBatch = floydWarshallOptimization(taskMasterBatch)
-        offset = 0
-
-        for rankOfProcessor in range(worldSize):
-            thisRows = rowsSent.pop(0)
-            if rankOfProcessor == TASK_MASTER:
-                costMatrix[offset:offset+thisRows] = taskMasterBatch
-
-            elif rankOfProcessor != TASK_MASTER:
-                data = comm.recv(source=rankOfProcessor, tag=rankOfProcessor)
-                costMatrix[offset:offset+thisRows] = data
-            
-            offset += thisRows
-        
-        print("\n== FINAL MATRIX ==\n")
-        print(costMatrix)
-        
-        
-
-    elif rank != TASK_MASTER:
-        rowsBatch = comm.recv(source=TASK_MASTER, tag=rank)
-        rowsBatch = floydWarshallOptimization(rowsBatch)
-        comm.send(rowsBatch, dest=TASK_MASTER, tag=rank)
 
 
 if __name__ == "__main__":
-    floydAlgorithm()
+    N = 4
+    floydAlgorithm(N)
     
